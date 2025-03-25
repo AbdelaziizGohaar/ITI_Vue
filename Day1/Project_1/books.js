79203a2b5c161b22846129c95be823297f17d0bb
AbdelaziizGohaar <abdelazizgohaar25@gmail.com>
2025-03-26 01:31:23 +0200

    THis is my Project 1 in Day 1 CDN Bookstore

---
+++ Day1/Project_1/books.js
@@ -0,0 +1,64 @@
+const books = [
+    { 
+      author: "Chinua Achebe",
+      imageLink: "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      link: "https://en.wikipedia.org/wiki/Things_Fall_Apart",
+      pages: 209,
+      title: "Things Fall Apart",
+      isbn: "978-0385474542",
+      category: "Fiction",
+      price: 15.99
+    },
+    {
+      author: "Hans Christian Andersen",
+      imageLink: "https://images.unsplash.com/photo-1544947950-fa07a98d237f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      link: "https://en.wikipedia.org/wiki/Fairy_Tales_Told_for_Children._First_Collection.",
+      pages: 784,
+      title: "Fairy Tales",
+      isbn: "978-0140448955",
+      category: "Children's Literature",
+      price: 12.99
+    },
+    {
+      author: "Dante Alighieri",
+      imageLink: "https://images.unsplash.com/photo-1509021436665-8f07dbf5bf1d?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      link: "https://en.wikipedia.org/wiki/Divine_Comedy",
+      pages: 928,
+      title: "The Divine Comedy",
+      isbn: "978-0142437223",
+      category: "Classic",
+      price: 18.50
+    },
+    {
+      author: "Jhon Steinbeck",
+      imageLink: "https://images.unsplash.com/photo-1476275466078-4007374efbbe?q=80&w=1429&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      link: "https://en.wikipedia.org/wiki/Epic_of_Gilgamesh",
+      pages: 160,
+      title: "The Epic Of Gilgamesh",
+      isbn: "978-0140441000",
+      category: "Mythology",
+      price: 14.99
+    },
+    {
+      author: "Halen C.S. Lewis",
+      imageLink: "https://images.unsplash.com/photo-1588666309990-d68f08e3d4a6?q=80&w=1370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      link: "https://en.wikipedia.org/wiki/Book_of_Job",
+      pages: 176,
+      title: "The Book Of Job",
+      isbn: "978-0192835001",
+      category: "Religious Text",
+      price: 10.99
+    },
+    {
+      author: "Sun Tzu",
+      imageLink: "https://images.unsplash.com/photo-1562232573-0305012a8818?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      link: "https://en.wikipedia.org/wiki/One_Thousand_and_One_Nights",
+      pages: 288,
+      title: "One Thousand and One Nights",
+      isbn: "978-0140442892",
+      category: "Folklore",
+      price: 16.99
+    }
+  ];
+  
+  //console.log(books);
